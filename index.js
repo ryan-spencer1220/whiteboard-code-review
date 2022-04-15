080e273d7d48b8e9b3b5032fd28e7b23af9fda9e
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-04-15 12:33:12 -0700

    WIP: Continue to add to recursion solutions

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-// Example
-// Input: [7, 9, "hi", 12, "hi", 7, 53]
+// function compressionString(string) {
+//   let result = ""
+//   let count = 0
 
-// Output: [7, 9, "hi", 12, 53]
+//   for(let i = 0; i < string.length; i++) {
+//     count++
+//     if(string[i] != string[i+1]) {
+//       result = result + string[i] + count;
+//       count = 0;
+//     }
+//   }
+//   return result.replace(/1/g, '');
+// }
 
-function answerFunction(array) {
-  answerArray = [];
-  for (let i = 0; i < array.length; i++) {
-    if (!answerArray.includes(array[i])) {
-      answerArray.push(array[i]);
-    }
+// RECURSION METHOD
+
+// let i = 0;
+// let answerArray = [];
+
+// const duplicatesRecursive = inputArray => {
+//   if (inputArray.length === 0) {
+//     return answerArray;
+//   } else if (!answerArray.includes(inputArray[i])) {
+//     answerArray.push(inputArray[i]);
+//     inputArray.shift();
+//     return duplicatesRecursive(inputArray);
+//   } else {
+//     inputArray.shift();
+//     return duplicatesRecursive(inputArray);
+//   }
+// };
+
+let result = "";
+let count = 0;
+let i = 0;
+
+function recursionString(string) {
+  count++;
+  if (string.length === 0) {
+    return answerArray;
+  } else if (string[i] != string[i + 1]) {
+    result = result + string[i] + count;
+    count = 0;
+    string = string.substring(1);
+    return recursionString(string);
+  } else {
+    string = string.substring(1);
+    return recursionString(string);
   }
-  return answerArray;
 }
+
+console.log(recursionString("aaabccdddda"));
